f9b34f6730f7448e3785baf0f84a0bf75b6dda9b
jcolinb <jessebaker@protonmail.com>
2018-09-27 12:18:37 -0400

    fix missing imageLinks bug in search function

--- src/BookFinder.js
+++ src/BookFinder.js
@@ -1,22 +1,47 @@
 import React,{Component} from 'react'
-import * from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 
 class BookFinder extends Component {
 
-    this.state = {
-	searchTerm: ''
+    state = {
+	searchTerm: '',
+	searchResults: []
     }
-    
+
+    updateTerm(term) {
+	(term &&
+	BooksAPI.search(term).then((data) => this.setState({searchTerm: (term),searchResults: ((data && !(data.error)) ? data : [])}))
+	) ||
+	(this.setState({searchTerm:''}))
+    }
+
     render() {
 	return (
-	    <div>
-	      <input
-	        type="text"
-	        placeholder="Search for books in library"
-	        value={this.state.searchTerm}
-	        onChange={(e) => this.setState({searchTerm: e.target.value})}
-	      />
-	    </div>
+	    	<div>
+		  <input
+	            type="text"
+	            value={this.state.searchTerm}
+	            onChange={(e) =>
+		      this.updateTerm(e.target.value)
+		    }
+		  />
+		<ol className="books-grid">
+
+	        {this.state.searchResults.map(function(book) {
+		  { if (book.imageLinks) {
+		     return (
+		       <li key={book.id}>
+		         <div className="book-cover"
+		              style={{width: 128,height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}
+			 ></div>
+		       </li>
+		    )
+		   }
+		  }
+		 })
+		}
+	        </ol>
+	      </div>
 	)
     }
 }
